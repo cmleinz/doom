9cbfa0041f6c3b85344de7229ac7aeb6f6f2438f
Caleb <cleinz+github@pm.me>
2023-12-13 21:19:54 -0800

    Add github copilot

--- config.el
+++ config.el
@@ -103,3 +103,11 @@
 
 
 (setq company-idle-delay 0.1)
+
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
